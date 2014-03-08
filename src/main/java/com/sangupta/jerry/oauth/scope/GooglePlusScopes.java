8994a6cdd0ea1cf614d5eb18317cb491da26d60b
Sandeep Gupta <sandy.pec@gmail.com>
2014-03-08 22:44:50 +0530

    Added scopes available for Google and Google Plus

---
+++ src/main/java/com/sangupta/jerry/oauth/scope/GooglePlusScopes.java
@@ -0,0 +1,9 @@
+package com.sangupta.jerry.oauth.scope;
+
+public interface GooglePlusScopes {
+	
+	public static final String LOGIN = "https://www.googleapis.com/auth/plus.login";
+
+	public static final String PROFILE_EMAILS_READ = "https://www.googleapis.com/auth/plus.profile.emails.read";
+	
+}
---
+++ src/main/java/com/sangupta/jerry/oauth/scope/GoogleScopes.java
@@ -0,0 +1,9 @@
+package com.sangupta.jerry.oauth.scope;
+
+public interface GoogleScopes {
+
+	public static final String PROFILE = "profile";
+	
+	public static final String EMAIL = "email";
+	
+}
